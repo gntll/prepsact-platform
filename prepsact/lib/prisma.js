0020877c84dbf513b6017ccc4546403b793a75e2
gntll <gottipatip@gmail.com>
2026-08-19 18:55:46 -0400

    Fix: Upgrade configuration for Prisma 7

--- prepsact/lib/prisma.js
+++ prepsact/lib/prisma.js
@@ -1,4 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
 const globalForPrisma = globalThis;
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// Initialize the Postgres Adapter using your connection pool URL
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+
+// Pass the adapter into the PrismaClient constructor
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
